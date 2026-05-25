533c4b38fca2f21c1837d2404564870ea31cdd63
jakob knudsen <jakobkn@gmail.com>
2026-05-25 13:51:30 +0200

    Change text on the frontend

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "this is from core-test!");
+app.MapGet("/", () => "this is from core-test automatic build!");
 
 app.Run();
