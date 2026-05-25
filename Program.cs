57fe09fbeb3f66dacb22c1110bdd857a04302de1
jakob knudsen <jakobkn@gmail.com>
2026-05-25 20:14:21 +0200

    Develop branch and build and release is added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "this is from core-test automatic build version 2!");
+app.MapGet("/", () => "this is from core-test automatic build when pushing to dev!");
  
 app.Run();
